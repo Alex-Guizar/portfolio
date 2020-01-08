89ec6c7c90ce62f6bad3a4eb964621819d088788
Alex Guizar <alexguizar90@gmail.com>
2020-01-07 20:33:36 -0800

    Added 2x image src to site array, and added srcset to image call

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,76 +6,87 @@ const Portfolio = () => {
       name: 'Gobbie Collectors',
       note: 'A FFXIV Fan Project',
       image: null,
+      image2x: null,
       link: null,
       id: 10
     },
     {
       name: 'Tennis Warehouse',
       note: null,
-      image: '/images/tennis-warehouse.png',
+      image: '/images/tennis-warehouse-1x.png',
+      image2x: '/images/tennis-warehouse.png',
       link: 'https://www.tennis-warehouse.com',
       id: 0
     },
     {
       name: 'Tennis Warehouse Europe',
       note: null,
-      image: '/images/tenniswarehouse-europe.png',
+      image: '/images/tenniswarehouse-europe-1x.png',
+      image2x: '/images/tenniswarehouse-europe.png',
       link: 'https://www.tenniswarehouse-europe.com',
       id: 1
     },
     {
       name: 'Tennis Only',
       note: null,
-      image: '/images/tennisonly.png',
+      image: '/images/tennisonly-1x.png',
+      image2x: '/images/tennisonly.png',
       link: 'https://www.tennisonly.com.au',
       id: 2
     },
     {
       name: 'Tackle Warehouse',
       note: null,
-      image: '/images/tacklewarehouse.png',
+      image: '/images/tacklewarehouse-1x.png',
+      image2x: '/images/tacklewarehouse.png',
       link: 'https://www.tacklewarehouse.com',
       id: 3
     },
     {
       name: 'Running Warehouse',
       note: null,
-      image: '/images/runningwarehouse.png',
+      image: '/images/runningwarehouse-1x.png',
+      image2x: '/images/runningwarehouse.png',
       link: 'https://www.runningwarehouse.com',
       id: 4
     },
     {
       name: 'Running Warehouse Europe',
       note: null,
-      image: '/images/runningwarehouse-eu.png',
+      image: '/images/runningwarehouse-eu-1x.png',
+      image2x: '/images/runningwarehouse-eu.png',
       link: 'https://www.runningwarehouse.eu',
       id: 5
     },
     {
       name: 'Running Warehouse Australia',
       note: null,
-      image: '/images/runningwarehouse-au.png',
+      image: '/images/runningwarehouse-au-1x.png',
+      image2x: '/images/runningwarehouse-au.png',
       link: 'https://www.runningwarehouse.com.au',
       id: 6
     },
     {
       name: 'Inline Warehouse',
       note: null,
-      image: '/images/inlinewarehouse.png',
+      image: '/images/inlinewarehouse-1x.png',
+      image2x: '/images/inlinewarehouse.png',
       link: 'https://www.inlinewarehouse.com',
       id: 7
     },
     {
       name: 'Skate Warehouse',
       note: null,
-      image: '/images/skatewarehouse.png',
+      image: '/images/skatewarehouse-1x.png',
+      image2x: '/images/skatewarehouse.png',
       link: 'https://www.skatewarehouse.com',
       id: 8
     },
     {
       name: 'Racquetball Warehouse',
       note: null,
-      image: '/images/racquetballwarehouse.png',
+      image: '/images/racquetballwarehouse-1x.png',
+      image2x: '/images/racquetballwarehouse.png',
       link: 'https://www.racquetballwarehouse.com',
       id: 9
     }
@@ -89,7 +100,7 @@ const Portfolio = () => {
         {websites.map( (website) =>
           <li key={website.id}>
             <span tabIndex="0" className="portfolio-list-image_wrap">
-              {website.image === null ? <span className="portfolio-list-image_wrap-temp">{website.name}</span> : <img className="portfolio-list-image" src={website.image} alt={website.name} />}
+              {website.image === null ? <span className="portfolio-list-image_wrap-temp">{website.name}</span> : <img className="portfolio-list-image" src={website.image} srcSet={website.image2x} sizes="275" alt={website.name} />}
               {website.note === null ? '' : <span className="portfolio-list-image_wrap-note">{website.note}</span>}
               <span className="portfolio-list-image_wrap-overlay">
                 <span>{website.link === null ? 'Coming Soon!' : website.name}</span>
