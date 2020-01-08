b3f497d8a7b9727ece4a15881f97d04cef71d4b7
Alex Guizar <alexguizar90@gmail.com>
2020-01-07 17:30:50 -0800

    Added scrollspy, sticky header, and reformatted css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollspyNav from 'react-scrollspy-nav';
 
 const Header = () => {
   return (
@@ -8,12 +9,19 @@ const Header = () => {
       </header>
 
       <nav id="top_nav">
-        <ul className="nav_list text-center list-reset">
-          <li><a href="#header">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
+        <ScrollspyNav
+          scrollTargetIds={['header', 'about', 'portfolio', 'contact']}
+          activeNavClass='is-active'
+          scrollDuration='500'
+          headerBackground='true'
+        >
+          <ul className="nav_list text-center list-reset">
+            <li><a href="#header">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </ScrollspyNav>
       </nav>
     </React.Fragment>
   );
